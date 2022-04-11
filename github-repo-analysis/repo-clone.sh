4b1722883667d6cc280bcc2db78d38cfe8e97f83
Karl <ksmunson@ucsc.edu>
2022-04-10 21:54:56 -0700

    added bash script for downloading top and bottom repos used

---
+++ github-repo-analysis/repo-clone.sh
@@ -0,0 +1,23 @@
+#! repos cloned
+
+# bottom repos
+git clone https://github.com/martyfuhry/npy_datetime.git
+git clone https://github.com/tonsV2/Gnome-menu-applet.git
+git clone https://github.com/mconlon17/vivo-remove-people.git
+git clone https://github.com/mwolak07/Calculator-Course-2019.git
+git clone https://github.com/mdamien/shopi.git
+
+
+# top repos
+# warning this is almost 2 gb of python files
+git clone https://github.com/vinta/awesome-python.git
+git clone https://github.com/ytdl-org/youtube-dl.git
+git clone https://github.com/django/django.git
+git clone https://github.com/pallets/flask.git
+git clone https://github.com/keras-team/keras.git
+git clone https://github.com/scikit-learn/scikit-learn.git
+git clone https://github.com/pandas-dev/pandas.git
+git clone https://github.com/numpy/numpy.git
+git clone https://github.com/scipy/scipy.git
+git clone https://github.com/nltk/nltk.git
+git clone https://github.com/pytorch/pytorch.git
