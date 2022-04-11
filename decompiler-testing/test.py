121338b4f86e8e7748c8fd59b444b4d9aadee3a9
serenity-wade <swade@hmc.edu>
2022-04-11 10:58:18 -0700

    added bottom repos to script

---
+++ decompiler-testing/test.py
@@ -0,0 +1,11 @@
+import numpy
+
+def function_name(par_1, parTwo, camelCase):
+	"""
+	docstring time
+	"""
+	var_1 = 42 # cool and awesome comment
+	print('hello world!') #comment too
+	return 
+
+function_name(1, 2, 3)
