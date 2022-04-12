c01ed6a3593c237ab141ee17a8751783c7366b10
Chih-Kai Ting <tingken0214@gmail.com>
2022-04-12 14:33:26 -0700

    py150k analysis

---
+++ py-150-analysis/test_code.py
@@ -0,0 +1,11 @@
+def function_name(par_1, parTwo, camelCase):
+	'''
+	docstring time 1
+	'''
+	"""
+	docstring time 2
+	"""
+    # single line comment
+	var_1 = 42 # cool and awesome comment
+	print('hello world!') #comment too
+	return 
