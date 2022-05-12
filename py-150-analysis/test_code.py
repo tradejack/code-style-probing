3bf474ce78aba2c958ea130b4e78637b6fc1b356
Chih-Kai Ting <tingken0214@gmail.com>
2022-05-11 22:42:13 -0700

    py150k analysis - investigating in how to extract metrics from AST in Py150k

--- py-150-analysis/test_code.py
+++ py-150-analysis/test_code.py
@@ -1,11 +1,45 @@
 def function_name(par_1, parTwo, camelCase):
-	'''
-	docstring time 1
-	'''
-	"""
+    """
+    docstring time 1
+    """
+    """
 	docstring time 2
 	"""
     # single line comment
-	var_1 = 42 # cool and awesome comment
-	print('hello world!') #comment too
-	return 
+    var_1 = 42  # cool and awesome comment
+    print("hello world!")  # comment too
+    return
+
+
+def function_name2(par_1, parTwo, camelCase):
+    """
+    docstring time 1
+    """
+    """
+	docstring time 2
+	"""
+    # single line comment
+    var_1 = 42  # cool and awesome comment
+    print("hello world!")  # comment too
+    return
+
+
+def input_code(nums):
+    """_summary_"""
+    results = []
+    for num in nums:
+        if num > 0:
+            results.append(num + 1)
+    return results
+
+
+def output_code(nums):
+    """_summary_
+
+    Args:
+        nums (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return [num + 1 for num in nums if num > 0]
