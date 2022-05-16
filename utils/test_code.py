f67f3cf147c92d05da4c07574211818a08e581e7
Chih-Kai Ting <tingken0214@gmail.com>
2022-05-16 15:36:06 -0700

    refactor(metric_extraction) - refactor python3 metric extraction - code from `github_gloss.py` and `ast_gloss.py`

---
+++ utils/test_code.py
@@ -0,0 +1,8 @@
+async def function_name(par_1, parTwo, camelCase):
+    """
+	docstring time
+	"""
+    var_1 = 42  # cool and awesome comment
+    print("hello world!")  # comment too
+    return
+
