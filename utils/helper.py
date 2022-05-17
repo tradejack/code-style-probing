de4d5d0551f317bf4137e2c148717c93d9c0313a
Chih-Kai Ting <tingken0214@gmail.com>
2022-05-16 22:11:34 -0700

    metric-extraction: script refactoring - merge py150k and python3 extraction code into the same file

--- utils/helper.py
+++ utils/helper.py
@@ -1,3 +1,5 @@
+from tqdm.auto import tqdm
+
 def read_file_to_string(filename):
     f = open(filename, "rb")
     s = ""
@@ -7,6 +9,32 @@ def read_file_to_string(filename):
         print(filename)
     f.close()
     return s.decode(errors="replace")
+
+
+def read_py150k_ast(filename, limit=None):
+    ast = []
+    count = 0
+    with open(filename) as json_file:
+        for line in tqdm(json_file):
+            ast.append(line)
+            count += 1
+            if limit and count >= limit:
+                break
+    return ast
+
+
+def read_py150k_code(filename, limit=None):
+    filenames = []
+    with open(filename, "r") as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            filenames += [line.strip()]
+            if limit and len(filenames) >= limit:
+                break
+
+    return filenames
 
 
 def calculate_ratio(numerator, denominator, round_val=6):
