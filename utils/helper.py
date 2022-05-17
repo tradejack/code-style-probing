efb9b496921c072eadc1afcbf9c844f496c87510
Chih-Kai Ting <tingken0214@gmail.com>
2022-05-16 22:45:53 -0700

    extract-metrics - fix casing counting error: token is None - code for output csv - modify readme for data extraction of Py150k instruction

--- utils/helper.py
+++ utils/helper.py
@@ -1,4 +1,6 @@
+import pandas as pd
 from tqdm.auto import tqdm
+
 
 def read_file_to_string(filename):
     f = open(filename, "rb")
@@ -39,3 +41,7 @@ def read_py150k_code(filename, limit=None):
 
 def calculate_ratio(numerator, denominator, round_val=6):
     return round(numerator / denominator, round_val) if denominator > 0 else 0
+
+
+def metric_dict_to_df(metric_dict_list):
+    return pd.DataFrame(metric_dict_list).fillna(0)
